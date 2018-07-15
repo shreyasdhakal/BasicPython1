a7d3cda8a155843b742c487771fb9fd51b41b018
shreyasdhakal <40919227+shreyasdhakal@users.noreply.github.com>
2018-07-15 08:45:39 +0545

    July7

---
+++ assignment.py
@@ -0,0 +1,17 @@
+v=25
+u=0
+t=10
+a=(v-u)/t
+print(a)
+
+f_name=input("Enter first name")
+l_name=input("Enter last name")
+full_name= f_name +""+ l_name
+print(full_name)
+
+user_input= input("Enter number")
+print(user_input *10)
+
+print(type(4))
+print(str(45))
+print(eval("2+3")
